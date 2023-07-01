37a9b6a91f94ad75bc674b83ee82eeaec3f02263
asaringo <ka99code@gmail.com>
2023-07-01 14:31:10 +0900

    [fix] template added

--- Math/Eratosthenes/Eratosthenes.cpp
+++ Math/Eratosthenes/Eratosthenes.cpp
@@ -1,17 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std ;
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math")
 typedef long long ll ;
 typedef long double ld ;
-typedef pair<ll,ll> P ;
-typedef tuple<ll,ll,ll> TP ;
 #define chmin(a,b) a = min(a,b)
 #define chmax(a,b) a = max(a,b)
 #define bit_count(x) __builtin_popcountll(x)
+#define leading_zero_count(x) __builtin_clz(x)
+#define trailing_zero_count(x) __builtin_ctz(x)
 #define gcd(a,b) __gcd(a,b)
 #define lcm(a,b) a / gcd(a,b) * b
 #define rep(i,n) for(int i = 0 ; i < n ; i++)
 #define rrep(i,a,b) for(int i = a ; i < b ; i++)
+#define repi(it,S) for(auto it = S.begin() ; it != S.end() ; it++)
+#define pt(a) cout << a << endl
+#define debug(a) cout << #a << " " << a << endl
+#define all(a) a.begin(), a.end()
 #define endl "\n"
+#define v1(n,a) vector<ll>(n,a)
+#define v2(n,m,a) vector<vector<ll>>(n,v1(m,a))
+#define v3(n,m,k,a) vector<vector<vector<ll>>>(n,v2(m,k,a))
+#define v4(n,m,k,l,a) vector<vector<vector<vector<ll>>>>(n,v3(m,k,l,a))
+template<typename T,typename U>istream &operator>>(istream&is,pair<T,U>&p){is>>p.first>>p.second;return is;}
+template<typename T,typename U>ostream &operator<<(ostream&os,const pair<T,U>&p){os<<p.first<<" "<<p.second;return os;}
+template<typename T>istream &operator>>(istream&is,vector<T>&v){for(T &in:v){is>>in;}return is;}
+template<typename T>ostream &operator<<(ostream&os,const vector<T>&v){for(auto it=v.begin();it!=v.end();){os<<*it<<((++it)!=v.end()?" ":"");}return os;}
+template<typename T>istream &operator>>(istream&is,vector<vector<T>>&v){for(T &in:v){is>>in;}return is;}
+template<typename T>ostream &operator<<(ostream&os,const vector<vector<T>>&v){for(auto it=v.begin();it!=v.end();){os<<*it<<((++it)!=v.end()?"\n":"");}return os;}
+template<typename T>ostream &operator<<(ostream&os,const set<T>&v){for(auto it=v.begin();it!=v.end();){os<<*it<<((++it)!=v.end()?" ":"");}return os;}
 
 struct Eratosthenes{
 
@@ -45,8 +62,8 @@ struct Eratosthenes{
         }
         
         // 素因数分解 20 -> { (5,1), (2,2) }
-        vector<P> prime_factorization_(int k){
-            vector<P> res ;
+        vector<pair<int,int>> prime_factorization_(int k){
+            vector<pair<int,int>> res ;
             while(k != 1){
                 int ex = 0 ;
                 int d = factor[k] ;
@@ -54,7 +71,7 @@ struct Eratosthenes{
                     k /= d ;
                     ex++ ;
                 }
-                res.push_back(P(d,ex)) ;
+                res.push_back(pair<int,int>(d,ex)) ;
             }
             return res ;
         }
@@ -122,7 +139,7 @@ struct Eratosthenes{
         Eratosthenes(){}
         Eratosthenes(int n_){ init_(n_); }
         void init(int n_) { init_(n_); }
-        vector<P> prime_factorization(int k) { return prime_factorization_(k); }
+        vector<pair<int,int>> prime_factorization(int k) { return prime_factorization_(k); }
         vector<int> prime_factor(int k) { return prime_factor_(k); }
         int get_euler_phi(int k) { return get_euler_phi_(k); }
         int get_mobius(int k) { return mobius[k]; }
